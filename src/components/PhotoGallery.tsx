36f85f981c8f07dfb720076a1044f54bd8e2bf6a
apalekseev <apalekseev@avito.ru>
2026-02-12 15:03:38 +0300

    fix: convert HEIC to JPG for browser compatibility

--- src/components/PhotoGallery.tsx
+++ src/components/PhotoGallery.tsx
@@ -2,20 +2,20 @@ import { useState, useEffect } from 'react'
 
 const PHOTOS = [
   'FullSizeRender.JPG',
-  'IMG_2564.HEIC',
+  'IMG_2564.JPG',
   'IMG_2745.JPG',
-  'IMG_2931.HEIC',
-  'IMG_2994.HEIC',
-  'IMG_3284.HEIC',
-  'IMG_3436.HEIC',
+  'IMG_2931.JPG',
+  'IMG_2994.JPG',
+  'IMG_3284.JPG',
+  'IMG_3436.JPG',
   'IMG_3461.JPG',
   'IMG_3480.JPG',
-  'IMG_4572.HEIC',
-  'IMG_4635.HEIC',
+  'IMG_4572.JPG',
+  'IMG_4635.JPG',
   'IMG_4950.JPG',
   'IMG_5232.JPG',
-  'IMG_6262.HEIC',
-  'IMG_7271.HEIC',
+  'IMG_6262.JPG',
+  'IMG_7271.JPG',
 ]
 
 export default function PhotoGallery() {
